9fd4b489cb25b65fc99e188d3b845b748f873b64
OyanibTech-iii <pacificooyanib@gmail.com>
2026-05-20 15:07:52 +0800

    feat: clear unneeded variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-# Map MYSQL_URL to DATABASE_URL if DATABASE_URL is not set (common on Railway)
-if [ -z "$DATABASE_URL" ] && [ -n "$MYSQL_URL" ]; then
-    export DATABASE_URL="$MYSQL_URL"
-    echo "Exported DATABASE_URL from MYSQL_URL"
-fi
-
 echo "Starting PHP-FPM..."
 php-fpm -F &
 PHP_PID=$!
